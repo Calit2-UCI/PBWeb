af40f2606bab841ce322204fb34bd28379e08dcb
Steve Shi <steveshi7@gmail.com>
2014-08-08 15:57:45 -0700

    Finished patient-access page

---
+++ views/patient_access.php
@@ -0,0 +1,33 @@
+	<!DOCTYPE html>
+
+	<html>
+	<head>
+		<link href="/PBWeb/css/foundation.css" rel="stylesheet" type="text/css">
+	</head>
+
+
+	<body>
+		<div id="img" style="text-align:center;">
+			<a href="/PBWeb/index.php"><img src="/PBWeb/img/choc_logo.gif" width="329" height="154"></a>
+			<h2>Patient Access</h2>
+		</div>
+		<br>
+
+		<div class="row">
+			<div class="large-4 large-centered columns">
+				<form>
+					<div class="row">
+						<label for="input_patient_id"> Patient ID </label>
+						<input id="input_patient_id" type="text" name="patient_id" required placeholder="Enter Patient ID"/>
+					</div>
+					<div class="row">
+						<div class="large-4 large-centered">
+							<button type="submit"  name="Submit" class="button tiny">Submit</button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</body>
+<html>
--- views/register.php
+++ views/register.php
@@ -7,7 +7,7 @@
 
 <body>
 	<div id="img" style="text-align:center;">
-			<a href="/PBWeb/index.php"><img src="/PBWeb/img/choc_logo.gif" width="329" height="154"></a>
+		<a href="/PBWeb/index.php"><img src="/PBWeb/img/choc_logo.gif" width="329" height="154"></a>
 		<h2>Register</h2>
 		<br>
 		<?php
@@ -32,30 +32,30 @@
 			<form method="post" action="register.php" name="registerform">
 				<div class="row">
 					<label for="first_name">First name</label>
-					<input id="first_name" class="login_input" type="text" name="first_name" required placeholder="First Name"/>
+					<input id="first_name"  type="text" name="first_name" required placeholder="First Name"/>
 				</div>
 				<div class="row">	
 					<label for="last_name">Last name</label>
-					<input id="last_name" class="login_input" type="text" name="last_name" required placeholder="Last Name"/>
+					<input id="last_name" type="text" name="last_name" required placeholder="Last Name"/>
 				</div>
 				<div class="row">	
 					<!-- the user name input field uses a HTML5 pattern check -->
 					<label for="login_input_username">Username</label>
-					<input id="login_input_username" class="login_input" type="text" pattern="[a-zA-Z0-9]{2,64}" name="user_name" required placeholder="Username"/>
+					<input id="login_input_username"  type="text" pattern="[a-zA-Z0-9]{2,64}" name="user_name" required placeholder="Username"/>
 				</div>
 				<div class="row">
 					<!-- the email input field uses a HTML5 email type check -->
 					<label for="login_input_email">Email</label>
-					<input id="login_input_email" class="login_input" type="email" name="user_email" required placeholder="Email"/>
+					<input id="login_input_email"  type="email" name="user_email" required placeholder="Email"/>
 				</div>
 				<div class="row">
 					<label for="login_input_password_new">Password (min. 6 characters)</label>
-					<input id="login_input_password_new" class="login_input" type="password" name="user_password_new" pattern=".{6,}" required autocomplete="off" placeholder="Password"/>
+					<input id="login_input_password_new"  type="password" name="user_password_new" pattern=".{6,}" required autocomplete="off" placeholder="Password"/>
 				</div>
 
 				<div class="row">
 					<label for="login_input_password_repeat">Repeat password</label>
-					<input id="login_input_password_repeat" class="login_input" type="password" name="user_password_repeat" pattern=".{6,}" required autocomplete="off" placeholder="Repeat Password"/>
+					<input id="login_input_password_repeat"  type="password" name="user_password_repeat" pattern=".{6,}" required autocomplete="off" placeholder="Repeat Password"/>
 				</div>
 				<br>
 				<div class="row">
@@ -70,7 +70,7 @@
 		<!-- backlink -->
 
 		<div class="large-6 large-centered">
-		<a href="index.php" class="button tiny"> Back to Login Page</a>
+			<a href="index.php" class="button tiny"> Back to Login Page</a>
 		</div>
 	</div>
 </body>
