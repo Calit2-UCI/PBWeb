62fffe31257a70271e3e25b3cf123d0eac84fcde
Steve Shi <steveshi7@gmail.com>
2014-08-08 13:53:13 -0700

    Redesigned login page
    switched over to foundation, added registration button

--- views/not_logged_in.php
+++ views/not_logged_in.php
@@ -1,60 +1,64 @@
-<!DOCTYPE html>
+	<!DOCTYPE html>
 
-<html>
-<head>
-	<link href="/PBWeb/css/pure-min.css" rel="stylesheet" type="text/css">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<style>
-		form {
-			width: 500px;
-			margin: 0 auto;
-		}
-	</style>
-</head>
+	<html>
+	<head>
+		<link href="/PBWeb/css/foundation.css" rel="stylesheet" type="text/css">
+	</head>
 
-<body>
-	<div id="img" style="text-align:center;">
-		<img src="/PBWeb/img/choc_logo.gif" width="329" height="154"> 
-		<h1>Account Login</h1>
 
-	<?php
-	// show potential errors / feedback (from login object)
-	if (isset($login)) {
-		if ($login->errors) {
-			foreach ($login->errors as $error) {
-				echo ' <p style="color:red">' . $error . '</p>';
+	<body>
+		<div id="img" style="text-align:center;">
+			<img src="/PBWeb/img/choc_logo.gif" width="329" height="154"> 
+			<h2>Account Login</h2>
+			<br>
+
+			<?php
+		// show potential errors / feedback (from login object)
+			if (isset($login)) {
+				if ($login->errors) {
+					foreach ($login->errors as $error) {
+						echo ' <p style="color:red">' . $error . '</p>';
+					}
+				}
+				if ($login->messages) {
+					foreach ($login->messages as $message) {
+						echo $message;
+					}
+				}
 			}
-		}
-		if ($login->messages) {
-			foreach ($login->messages as $message) {
-				echo $message;
-			}
-		}
-	}
-	?>
-	</div>
-			
-	<!-- login form box -->
-	<form method="post" action="index.php" name="loginform"  class="pure-form pure-form-aligned">
-		<fieldset>
-			<div class="pure-control-group">
-				<label for="login_input_username">Username</label>
-				<input id="login_input_username" class="login_input" type="text" name="user_name" required placeholder="Username"/>
-			</div>
+			?>
+		</div>
 
-			<div class="pure-control-group">		
-				<label for="login_input_password">Password</label>
-				<input id="login_input_password" class="login_input" type="password" name="user_password" autocomplete="off" required placeholder="Password"/>
-			</div>
+		<div class="row">
+			<div class="large-4 large-centered columns">
+				<form method="post" action="index.php" name="loginform">
+					<div class="row">
+						<label for="login_input_username"> Username </label>
+						<input id="login_input_username" class="login_input" type="text" name="user_name" required placeholder="Username"/>
+					</div>
+					<div class="row">
+						<label for="login_input_password">Password</label>
+						<input id="login_input_password" class="login_input" type="password" name="user_password" autocomplete="off" required placeholder="Password"/>
+					</div>
+					<!-- <input type="submit"  name="login" value="Log in" /> -->
+					<br>
+					<div class="row">
+						<div class="large-12 large-centered columns">
+							<div class="row">
+								<div class="large-6 columns">
+									<button type="submit"  name="login" class="button">Log in</button>
+								</div>
+								<div class="large-6 columns">
+									<a href="register.php" class="button [secondary success alert]">Register</a>
+								</div>
+							</div>
+						</div>
+					</div> 
+				</div>
+			</form>
+		</div>
 
-			<div class="pure-controls">	
-				<!-- <input type="submit"  name="login" value="Log in" /> -->
-				<button type="submit"  name="login" class="pure-button pure-button-primary">Log in</button>
-			</div>
-		</fieldset>
 
-	</form>
 
-	<a href="register.php"  style="text-align:center;">Register new account</a>
-</body>
-<html>
+	</body>
+	<html>
