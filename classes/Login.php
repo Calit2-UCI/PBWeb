ca356c8278c73b9e6c4f7406695cb9a09f441743
Tianrui Guo <tianruig@gmail.com>
2014-08-07 19:00:17 -0700

    Login: fix table and column names

--- classes/Login.php
+++ classes/Login.php
@@ -67,9 +67,9 @@ class Login
 
                 // database query, getting all the info of the selected user (allows login via email address in the
                 // username field)
-                $sql = "SELECT user_name, user_email, user_password_hash
-                        FROM users
-                        WHERE user_name = '" . $user_name . "' OR user_email = '" . $user_name . "';";
+                $sql = "SELECT username, email, password
+                        FROM user_login
+                        WHERE username = '" . $user_name . "' OR email = '" . $user_name . "';";
                 $result_of_login_check = $this->db_connection->query($sql);
 
                 // if this user exists
@@ -80,11 +80,11 @@ class Login
 
                     // using PHP 5.5's password_verify() function to check if the provided password fits
                     // the hash of that user's password
-                    if (password_verify($_POST['user_password'], $result_row->user_password_hash)) {
+                    if (password_verify($_POST['user_password'], $result_row->password)) {
 
                         // write user data into PHP SESSION (a file on your server)
-                        $_SESSION['user_name'] = $result_row->user_name;
-                        $_SESSION['user_email'] = $result_row->user_email;
+                        $_SESSION['user_name'] = $result_row->username;
+                        $_SESSION['user_email'] = $result_row->email;
                         $_SESSION['user_login_status'] = 1;
 
                     } else {
