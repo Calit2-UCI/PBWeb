d222b3cdb67e5c3ede02e7a2e7d8b358e3c89981
thevises <steveshi7@gmail.com>
2014-08-08 19:37:03 -0700

    Fixed pathing, deleted unnecessary tags

--- views/logged_in.php
+++ views/logged_in.php
@@ -8,13 +8,13 @@
 	<title>Foundation 5</title>
 
 	<!-- If you are using the CSS version, only link these 2 files, you may add app.css to use for your overrides if you like -->
-	<link rel="stylesheet" href="css/normalize.css">
-	<link rel="stylesheet" href="css/foundation.css">
+	<link rel="stylesheet" href="/PBWeb/css/normalize.css">
+	<link rel="stylesheet" href="/PBWeb/css/foundation.css">
 
 	<!-- If you are using the gem version, you need this only -->
-	<link rel="stylesheet" href="css/app.css">
+	<link rel="stylesheet" href="/PBWeb/css/app.css">
 
-	<script src="js/vendor/modernizr.js"></script>
+	<script src="/PBWeb/js/vendor/modernizr.js"></script>
 
 </head>
 <body>
--- views/patient_access.php
+++ views/patient_access.php
@@ -1,8 +1,19 @@
 <!DOCTYPE html>
+<html class="no-js" lang="en" >
+<<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Foundation 5</title>
 
-<html>
-<head>
-	<link href="/PBWeb/css/foundation.css" rel="stylesheet" type="text/css">
+<!-- If you are using the CSS version, only link these 2 files, you may add app.css to use for your overrides if you like -->
+<link rel="stylesheet" href="/PBWeb/css/normalize.css">
+<link rel="stylesheet" href="/PBWeb/css/foundation.css">
+
+<!-- If you are using the gem version, you need this only -->
+<link rel="stylesheet" href="/PBWeb/css/app.css">
+
+<script src="/PBWeb/js/vendor/modernizr.js"></script>
+
 </head>
 
 
@@ -12,22 +23,21 @@
 	</div>
 	<div style="text-align:center;">
 		<h2>Patient Access</h2>
-        </div>
-		<br>
-		<div class="row">
-			<div class="small-4 small-centered columns">
-				<form>
-					<div class="row">
-						<label for="input_patient_id"> Patient ID </label>
-						<input id="input_patient_id" type="text" name="patient_id" required placeholder="Enter Patient ID"/>
+	</div>
+	<br>
+	<div class="row">
+		<div class="small-4 small-centered columns">
+			<form>
+				<div class="row">
+					<label for="input_patient_id"> Patient ID </label>
+					<input id="input_patient_id" type="text" name="patient_id" required placeholder="Enter Patient ID"/>
+				</div>
+				<div class="row">
+					<div class="small-4 small-centered columns">
+						<button type="submit"  name="Submit" class="button tiny">Submit</button>
 					</div>
-					<div class="row">
-						<div class="small-4 small-centered columns">
-							<button type="submit"  name="Submit" class="button tiny">Submit</button>
-						</div>
-					</div>
-				</form>
-			</div>
+				</div>
+			</form>
 		</div>
 	</div>
 </body>
