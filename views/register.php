b2c6ef1cd1fbcdbe6c7d4d9ce1c653d797db68eb
thevises <steveshi7@gmail.com>
2014-08-08 20:58:21 -0700

    Fix Tabindex

--- views/register.php
+++ views/register.php
@@ -48,11 +48,11 @@
 
 			<div class="small-4 push-2 columns">
 				<label for="first_name">First name</label>
-				<input id="first_name"  type="text" name="first_name" required placeholder="First Name"/>
+				<input id="first_name"  type="text" tabindex=1 name="first_name" required placeholder="First Name"/>
 			</div>
 			<div class="small-4 pull-2 columns">
 				<label for="login_input_username">Username</label>
-				<input id="login_input_username"  type="text" pattern="[a-zA-Z0-9]{2,64}" name="user_name" required placeholder="Username"/>
+				<input id="login_input_username"  tabindex=4 type="text" pattern="[a-zA-Z0-9]{2,64}" name="user_name" required placeholder="Username"/>
 			</div>
 		</div>
 
@@ -60,11 +60,11 @@
 
 			<div class="small-4 push-2 columns">
 				<label for="last_name">Last name</label>
-				<input id="last_name" type="text" name="last_name" required placeholder="Last Name"/>
+				<input id="last_name" type="text" tabindex=2 name="last_name" required placeholder="Last Name"/>
 			</div>
 			<div class="small-4 pull-2 columns">
 				<label for="login_input_password_new">Password (min. 6 characters)</label>
-				<input id="login_input_password_new"  type="password" name="user_password_new" pattern=".{6,}" required autocomplete="off" placeholder="Password"/>
+				<input id="login_input_password_new" tabindex=5 type="password" name="user_password_new" pattern=".{6,}" required autocomplete="off" placeholder="Password"/>
 			</div>
 		</div>
 
@@ -72,19 +72,20 @@
 
 			<div class="small-4 push-2 columns">
 				<label for="login_input_email">Email</label>
-				<input id="login_input_email"  type="email" name="user_email" required placeholder="Email"/>
+				<input id="login_input_email"  tabindex=3 type="email" name="user_email" required placeholder="Email"/>
 			</div>
 			<div class="small-4 pull-2 columns">
 				<label for="login_input_password_repeat">Repeat password</label>
-				<input id="login_input_password_repeat"  type="password" name="user_password_repeat" pattern=".{6,}" required autocomplete="off" placeholder="Repeat Password"/>
+				<input id="login_input_password_repeat" tabindex=6 type="password" name="user_password_repeat" pattern=".{6,}" required autocomplete="off" placeholder="Repeat Password"/>
 			</div>
 		</div>
 
+		<br>
 		<div class="row">
-		<div class="small-2 columns small-push-4">
+		<div class="small-1 columns small-push-3">
 				<button type="submit"  name="login" class="button [secondary success alert">Register</button>
 			</div>
-			<div class="small-2 columns small-pull-4">
+			<div class="small-1 columns small-pull-4">
 				<a href="/PBWeb/index.php" class="button">Return</a>
 			</div>
 
