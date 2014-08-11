1fb2e0dc8ac3e70e10e9c88ed0f5327df76387fb
Tianrui Guo <tianruig@gmail.com>
2014-08-11 14:52:46 -0700

    cleanup

--- patient.php
+++ patient.php
@@ -38,7 +38,7 @@ $login = new Login();
 if ($login->isUserLoggedIn() == true) {
     // the user is logged in. you can do whatever you want here.
     // for demonstration purposes, we simply show the "you are logged in" view.
-    include("views/logged_in.php");
+    include("views/patient_access.php");
 
 } else {
     // the user is not logged in. you can do whatever you want here.
--- views/patient_access.php
+++ views/patient_access.php
@@ -1,46 +1,28 @@
-<!DOCTYPE html>
-<html class="no-js" lang="en" >
-
-<head>
-	<meta charset="utf-8" />
-	<!-- if you remove this meta tag, the NSA will spy on you through your Xbox Kinect camera -->
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<title>Foundation</title>
-	<link rel="stylesheet" href="/PBWeb/css/app.css" />
-	<script src="/PBWeb/bower_components/modernizr/modernizr.js"></script>
-
-</head>
-
-<body>
-	<div id="img" style="text-align:center;">
-		<a href="/PBWeb/index.php"><img src="/PBWeb/img/choc_logo.gif" width="494" height="231"></a>
-	</div>
-	<div class="small-12 large-6 small-centered columns">
-		<div class="panel">
-			<div style="text-align:center" id="box-shadow-default">
-				<br>
-				<h2>Patient Lookup</h2>
-			</div>
+<?php include('_header.php'); ?>
+<div class="small-12 large-6 small-centered columns">
+	<div class="panel">
+		<div style="text-align:center" id="box-shadow-default">
 			<br>
-
+			<h2>Patient Lookup</h2>
+		</div>
+		<br>
 			<form method="post" action="..\classes\patients.php" name="patientacess">
-				<div class="row">
-					<div class="small-8 large-4 small-centered columns">
-						<div class="row">
-							<label for="patient_id"> Patient ID </label>
-							<input id="patient_id" type="text" name="patient_id" required placeholder="Enter Patient ID"/>
-						</div>
-<br>
-						<div class="row">
-							<button type="submit"  name="Submit" class="button tiny expand">Submit</button>
-						</div>
-						<div class="row">
-							<a href="/PBWeb/index.php" class="button success tiny expand">Back</a>
-						</div>
+			<div class="row">
+				<div class="small-8 large-4 small-centered columns">
+					<div class="row">
+						<label for="patient_id"> Patient ID </label>
+						<input id="patient_id" type="text" name="patient_id" required placeholder="Enter Patient ID"/>
+					</div>
+					<br>
+					<div class="row">
+						<button type="submit"  name="Submit" class="button tiny expand">Submit</button>
+					</div>
+					<div class="row">
+						<a href="/PBWeb/index.php" class="button success tiny expand">Back</a>
 					</div>
 				</div>
-			</form>
-		</div>
+			</div>
+		</form>
 	</div>
-</body>
-<html>
+</div>
+<?php include('_footer.php'); ?>
