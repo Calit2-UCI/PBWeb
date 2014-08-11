6211db1cca6da132ed6b8f8b6a5ac67c9c5db11e
Tianrui Guo <tianruig@gmail.com>
2014-08-11 12:01:23 -0700

    Update header

--- views/_header.php
+++ views/_header.php
@@ -1,31 +1,16 @@
 <!DOCTYPE html>
-<html>
+
+<!--[if IE 9]><html class="lt-ie10" lang="en" > <![endif]-->
+<html class="no-js" lang="en" >
+
 <head>
-    <meta charset="UTF-8">
-    <title>php-login-advanced</title>
-    <style type="text/css">
-        /* just for the demo */
-        body {
-            font-family: Arial, sans-serif;
-            font-size: 12px;
-            margin: 10px;
-        }
-        label {
-            position: relative;
-            vertical-align: middle;
-            bottom: 1px;
-        }
-        input[type=text],
-        input[type=password],
-        input[type=submit],
-        input[type=email] {
-            display: block;
-            margin-bottom: 15px;
-        }
-        input[type=checkbox] {
-            margin-bottom: 15px;
-        }
-    </style>
+	<meta charset="utf-8" />
+	<!-- if you remove this meta tag, the NSA will spy on you through your Xbox Kinect camera -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>Painbuddy Web</title>
+	<link rel="stylesheet" href="/PBWeb/css/app.css" />
+	<script src="/PBWeb/bower_components/modernizr/modernizr.js"></script>
+	
 </head>
 <body>
 
@@ -34,12 +19,18 @@
 if (isset($login)) {
     if ($login->errors) {
         foreach ($login->errors as $error) {
+			echo '<div data-alert class="alert-box alert radius">';
             echo $error;
+			echo '<a href="#" class="close">&times;</a>
+				</div>';
         }
     }
     if ($login->messages) {
         foreach ($login->messages as $message) {
+			echo '<div data-alert class="alert-box info radius">';
             echo $message;
+			echo '<a href="#" class="close">&times;</a>
+				</div>';
         }
     }
 }
@@ -50,12 +41,18 @@ if (isset($login)) {
 if (isset($registration)) {
     if ($registration->errors) {
         foreach ($registration->errors as $error) {
+			echo '<div data-alert class="alert-box alert radius">';
             echo $error;
+			echo '<a href="#" class="close">&times;</a>
+				</div>';
         }
     }
     if ($registration->messages) {
         foreach ($registration->messages as $message) {
+			echo '<div data-alert class="alert-box info radius">';
             echo $message;
+			echo '<a href="#" class="close">&times;</a>
+				</div>';
         }
     }
 }
