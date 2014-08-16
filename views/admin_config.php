f95989f64239233c70127745052797283881d8c2
Tianrui Guo <tianruig@gmail.com>
2014-08-15 20:34:52 -0700

    Cleanup

--- views/admin_config.php
+++ views/admin_config.php
@@ -8,20 +8,19 @@
     <li class="tab-title"><a href="index.php">Back</a></li>
 </ul>
 <div class="tabs-content vertical">
-  <div class="content active" id="panel1a">
-      <h1>Welcome to the Administrator Configuration Page.</h1>
-      <h2> Use the tabs on the side to navigate.</h2>
-  </div>
-  <div class="content" id="panel2a">
-  <?php $admin->printPendingUsers(); ?>
+    <div class="content active" id="panel1a">
+        <h1>Welcome to the Administrator Configuration Page.</h1>
+        <h2> Use the tabs on the side to navigate.</h2>
+    </div>
+    <div class="content" id="panel2a">
+        <p><?php $admin->printPendingUsers(); ?></p>
+    </div>
+    <div class="content" id="panel3a">
+        <p>Panel 3 content goes here.</p>
+    </div>
+    <div class="content" id="panel4a">
+        <p>Panel 4 content goes here.</p>
+    </div>
 </div>
-<div class="content" id="panel3a">
-    <p>Panel 3 content goes here.</p>
-</div>
-<div class="content" id="panel4a">
-    <p>Panel 4 content goes here.</p>
-</div>
-
-
 
 <?php include('_footer.php'); ?>
