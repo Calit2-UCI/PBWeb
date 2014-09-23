06ee5034b0f6a7731a039dcfd9b1da789b29e58c
Tianrui Guo <tianruig@gmail.com>
2014-09-22 20:02:54 -0700

    Revert "Editing Admin Username"
    This reverts commit 76294001c375c98b00f9914006612cc388f9600a.

--- views/edit.php
+++ views/edit.php
@@ -16,7 +16,7 @@
                         <h5><?php echo WORDING_CHANGE_EMAIL ?></h5>
                     </div>
                     <br/>
-
+                
                     <label for="user_email"><?php echo WORDING_NEW_EMAIL; ?></label>
                     <input id="user_email" type="email" name="user_email" required placeholder="<?php echo WORDING_CURRENTLY; ?>: <?php echo $_SESSION['user_email']; ?>" />
                     <small class="error">Invalid Email</small>
@@ -54,14 +54,16 @@
                 <div class="row">
                     <input type="submit" class="button success expand" name="user_edit_submit_password" value="<?php echo WORDING_CHANGE_PASSWORD; ?>" />
                 </div>
-
             </form>
-            <div class="row">
-                <a href="index.php" class="button expand">Return to Menu</a>
-            </div>
         </div>
         <br>
     </div>
 </div>
 
+<div class="row">
+    <a href="index.php" class="button expand">Menu</a>
+</div>
+<div class="row">
+    <a href="index.php?logout" class="button alert expand"><?php echo WORDING_LOGOUT; ?></a>
+</div>
 <?php include('_footer.php'); ?>
