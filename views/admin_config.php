753bb512f9b0c51f632cfdbbacf3331b90d29545
thevises <steveshi7@gmail.com>
2014-09-22 19:52:43 -0700

    Fix Admin Config Implementation

--- views/admin_config.php
+++ views/admin_config.php
@@ -17,12 +17,9 @@
             <p><?php $admin->printActiveUsers(); ?></p>
         </div>
         <div class="row">
-            <a href="index.php" class="button expand">Menu</a>
-        </div>
-        <div class="row">
-            <a href="index.php?logout" class="button alert expand"><?php echo WORDING_LOGOUT; ?></a>
+            <a href="index.php" class="button expand">Return to Menu</a>
         </div>
     </div>
 </div>
-    
+
 <?php include('_footer.php'); ?>
