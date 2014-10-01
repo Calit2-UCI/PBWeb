4ccbb8b0fe74276acf9c83a9e5155a01b6255b22
Tianrui Guo <tianruig@gmail.com>
2014-09-30 18:39:09 -0700

    More cleanup

--- views/admin_config.php
+++ views/admin_config.php
@@ -10,46 +10,50 @@
   <div class="large-14 columns">
     <div class="callout panel">
       <h3>Pending Users</h3>
+
       <p><?php $admin->printPendingUsers(); ?></p>
     </div>
     <div class="callout panel">
       <h3>Active Users</h3>
+
       <p><?php $admin->printActiveUsers(); ?></p>
     </div>
     <div class="callout panel">
       <h3>Patients</h3>
+
       <p><?php $admin->printPatients(); ?></p>
     </div>
+    <div class="callout panel">
+      <div class="small-10 large-8 small-centered columns">
+        <form data-abide method="post" action="admin.php" name="admin_add_patient">
+          <div class="row">
+            <div style="text-align:center">
+              <h5><?php echo WORDING_ADMIN_ADD_PATIENT ?></h5>
+            </div>
 
-    <div class="small-10 large-8 small-centered columns">
-      <form data-abide method="post" action="admin.php" name="admin_add_patient">
-        <div class="row">
-          <div style="text-align:center">
-            <h5><?php echo WORDING_ADMIN_ADD_PATIENT ?></h5>
-          </div>
-
-          <label for="patient_first_name">First Name</label>
-            <input id="patient_first_name" type="text" name="patient_first_name" required placeholder="First Name" />
+            <label for="patient_first_name">First Name</label>
+            <input id="patient_first_name" type="text" name="patient_first_name" required placeholder="First Name"/>
             <small class="error">Invalid First Name</small>
 
-          <label for="patient_last_name">Last Name</label>
-            <input id="patient_last_name" type="text" name="patient_last_name" required placeholder="Last Name" />
+            <label for="patient_last_name">Last Name</label>
+            <input id="patient_last_name" type="text" name="patient_last_name" required placeholder="Last Name"/>
             <small class="error">Invalid Last Name</small>
 
-          <label for="patient_birth_date">Birth Date</label>
-            <input id="patient_birth_date" type="date" name="patient_birth_date" required placeholder="MM/DD/YYYY" />
+            <label for="patient_birth_date">Birth Date</label>
+            <input id="patient_birth_date" type="date" name="patient_birth_date" required placeholder="MM/DD/YYYY"/>
             <small class="error">Invalid birth date</small>
 
-          <!-- Doctor -->
-          <label for="patient_doctor">Doctor</label>
+            <!-- Doctor -->
+            <label for="patient_doctor">Doctor</label>
             <select id="patient_doctor" name="patient_doctor" required>
               <?php $admin->printUserOptions() ?>
               <small class="error">Select a doctor</small>
             </select>
 
-          <input type="submit" class="button success expand" name="admin_add_patient_submit" value="Add Patient" />
-        </div>
-      </form>
+            <input type="submit" class="button success expand" name="admin_add_patient_submit" value="Add Patient"/>
+          </div>
+        </form>
+      </div>
     </div>
 
     <div class="row">
