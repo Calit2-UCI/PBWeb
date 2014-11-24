76b6e6e46abab2ac337c37308864f357d2ac0705
thevises <steveshi7@gmail.com>
2014-11-23 20:12:31 -0800

    Button Grid Fixes

--- views/admin/edit_HCP.php
+++ views/admin/edit_HCP.php
@@ -22,77 +22,76 @@
 
           <label for="user_email"><?php echo WORDING_NEW_EMAIL; ?></label>
           <input id="user_email" type="email" name="user_email" required
-                 placeholder="<?php echo WORDING_CURRENTLY; ?>: <?php echo $admin->getUserEmail($_GET['edit_user']) ?>"/>
+          placeholder="<?php echo WORDING_CURRENTLY; ?>: <?php echo $admin->getUserEmail($_GET['edit_user']) ?>"/>
           <small class="error">Invalid Email</small>
 
           <button type="submit" class="button success expand" name="admin_edit_submit_email"
-                  value="<?php echo $_GET['edit_user']; ?>">Change Email
-          </button>
-        </div>
-      </form>
+          value="<?php echo $_GET['edit_user']; ?>">Change Email
+        </button>
+      </div>
+    </form>
 
-      <hr/>
+    <hr/>
 
-      <!-- edit username -->
-      <form data-abide method="post" action="admin.php" name="admin_edit_form_username">
-        <div class="row">
-          <div style="text-align:center">
-            <h5><?php echo WORDING_CHANGE_USERNAME ?></h5>
-          </div>
-
-          <br/>
-
-          <label for="user_name"><?php echo WORDING_NEW_USERNAME; ?></label>
-          <input id="user_name" type="text" name="user_name" required
-                 placeholder="<?php echo WORDING_CURRENTLY; ?>: <?php echo $admin->getUserUsername($_GET['edit_user']); ?>"/>
-          <small class="error">Invalid Username</small>
-
-          <button type="submit" class="button success expand" name="admin_edit_submit_username"
-                  value="<?php echo $_GET['edit_user']; ?>">Change Username
-          </button>
-        </div>
-      </form>
-
-      <hr/>
-
-      <!-- edit password -->
-      <form data-abide method="post" action="admin.php" name="admin_edit_form_password">
+    <!-- edit username -->
+    <form data-abide method="post" action="admin.php" name="admin_edit_form_username">
+      <div class="row">
         <div style="text-align:center">
-          <h5><?php echo WORDING_CHANGE_PASSWORD; ?></h5>
+          <h5><?php echo WORDING_CHANGE_USERNAME ?></h5>
         </div>
 
-        <div class="row">
-          <label for="user_password_new"><?php echo WORDING_NEW_PASSWORD; ?></label>
-          <input id="user_password_new" type="password" name="user_password_new" pattern=".{6,}" required
-                 autocomplete="off" placeholder="New Password"/>
-          <small class="error">Password must be at least 6 characters</small>
-        </div>
+        <br/>
 
-        <div class="row">
-          <label for="user_password_repeat"><?php echo WORDING_NEW_PASSWORD_REPEAT; ?></label>
-          <input id="user_password_repeat" type="password" name="user_password_repeat" pattern=".{6,}" required
-                 autocomplete="off" placeholder="Repeat Password" data-equalto="user_password_new"/>
-          <small class="error">Passwords must match</small>
-        </div>
+        <label for="user_name"><?php echo WORDING_NEW_USERNAME; ?></label>
+        <input id="user_name" type="text" name="user_name" required
+        placeholder="<?php echo WORDING_CURRENTLY; ?>: <?php echo $admin->getUserUsername($_GET['edit_user']); ?>"/>
+        <small class="error">Invalid Username</small>
 
-        <div class="row">
-          <button type="submit" class="button success expand" name="admin_edit_submit_password"
-                  value="<?php echo $_GET['edit_user']; ?>">Change Password
-          </button>
-        </div>
-      </form>
+        <button type="submit" class="button success expand" name="admin_edit_submit_username"
+        value="<?php echo $_GET['edit_user']; ?>">Change Username
+      </button>
     </div>
-  </div>
+  </form>
+
+  <hr/>
+
+  <!-- edit password -->
+  <form data-abide method="post" action="admin.php" name="admin_edit_form_password">
+    <div style="text-align:center">
+      <h5><?php echo WORDING_CHANGE_PASSWORD; ?></h5>
+    </div>
+
+    <div class="row">
+      <label for="user_password_new"><?php echo WORDING_NEW_PASSWORD; ?></label>
+      <input id="user_password_new" type="password" name="user_password_new" pattern=".{6,}" required
+      autocomplete="off" placeholder="New Password"/>
+      <small class="error">Password must be at least 6 characters</small>
+    </div>
+
+    <div class="row">
+      <label for="user_password_repeat"><?php echo WORDING_NEW_PASSWORD_REPEAT; ?></label>
+      <input id="user_password_repeat" type="password" name="user_password_repeat" pattern=".{6,}" required
+      autocomplete="off" placeholder="Repeat Password" data-equalto="user_password_new"/>
+      <small class="error">Passwords must match</small>
+    </div>
+
+    <div class="row">
+      <button type="submit" class="button success expand" name="admin_edit_submit_password"
+      value="<?php echo $_GET['edit_user']; ?>">Change Password</button>
+    </div>
+
+    <div class="row">
+      <a href="admin.php" class="button expand">Back To User List</a>
+    </div>
+    <div class="row">
+      <a href="index.php" class="button expand">Menu</a>
+    </div>
+
+  </form>
+</div>
+</div>
 </div>
 
-<div class="row">
-  <a href="admin.php" class="button expand">Back To User List</a>
-</div>
-<div class="row">
-  <a href="index.php" class="button expand">Menu</a>
-</div>
-<div class="row">
-  <a href="index.php?logout" class="button alert expand"><?php echo WORDING_LOGOUT; ?></a>
-</div>
+
 
 <?php include('/views/_footer.php'); ?>
