2d4a90a739716cb0bdc3df7c57380741cf17f09e
Tianrui Guo <tianruig@gmail.com>
2014-12-02 23:23:49 -0800

    WIP2

--- app/submit.php
+++ app/submit.php
@@ -4,6 +4,21 @@ require_once('../config/config.php');
 
 $db_connection = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8', DB_USER, DB_PASS);
 
+$query1 = $db_connection->prepare("CREATE TABLE IF NOT EXISTS `painbuddy`.`patient_responses1` (
+`response_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'unique id for each question response',
+`patient_id` int(11) NOT NULL COMMENT 'ID of the patient',
+`day` int(11) NOT NULL COMMENT 'day number',
+`ampm` ENUM('am','pm') NOT NULL COMMENT 'am or pm survey',
+`question_number` int(11) NOT NULL COMMENT 'question number',
+`major` int(1) NOT NULL COMMENT 'response to the major question (1 = yes, 0 = no)',
+`minor1` varchar(255) DEFAULT '*' COMMENT 'first minor question (* if not applicable)',
+`minor2` varchar(1) DEFAULT '*' COMMENT 'second minor question (* if not applicable)',
+`minor3` varchar(1) DEFAULT '*' COMMENT 'third minor question (* if not applicable)',
+`submit_time` DATETIME NOT NULL COMMENT 'time when survey was submitted (from now() when inserting records into database)',
+PRIMARY KEY (`response_id`)
+) AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='patient responses for section 1'");
+$query1->execute();
+
 /**
  * @param $db_connection
  * @param $patient_id
@@ -14,7 +29,7 @@ function process_response1($db_connection, $patient_id, $day, $ampm)
 {
   $response1_array = explode(",", $_POST['response1']);
   $response1_count = count($response1_array);
-  $response1_query = 'INSERT INTO TABLE1 (patient_id, day, ampm, submit_time, question_number, major, minor1, minor2, minor3) VALUES';
+  $response1_query = 'INSERT INTO patient_responses1 (patient_id, day, ampm, submit_time, question_number, major, minor1, minor2, minor3) VALUES';
   for ($i = 1; $i <= $response1_count; ++$i) {
     $response1_query .= " (:patient_id_{$i}, :day_{$i}, :ampm_{$i}, now(), :question_number_{$i}, :major_{$i}, :minor1_{$i}, :minor2_{$i}, :minor3_{$i}),";
   }
@@ -39,6 +54,7 @@ function process_response1($db_connection, $patient_id, $day, $ampm)
       $query_response1->bindValue(':minor3_' . $i, $response1[3], PDO::PARAM_STR);
     }
     $query_response1->execute();
+//    $query_response1->debugDumpParams();
   } catch (Exception $e) {
     echo($e->getMessage());
   }
@@ -53,9 +69,9 @@ function process_response1($db_connection, $patient_id, $day, $ampm)
 function process_response1_input($db_connection, $patient_id, $day, $ampm)
 {
   $response1_input_array = explode(",", $_POST['response1_input']);
-  print_r($response1_input_array);
+//  print_r($response1_input_array);
   try {
-    $query_response1_input = $db_connection->prepare('INSERT INTO TABLE1 (patient_id, day, ampm, submit_time, question_number, major, minor1, minor2, minor3) VALUES (:patient_id, :day, :ampm, now(), 99, :major, :minor1, *, *)');
+    $query_response1_input = $db_connection->prepare('INSERT INTO patient_responses1 (patient_id, day, ampm, submit_time, question_number, major, minor1, minor2, minor3) VALUES (:patient_id, :day, :ampm, now(), 99, :major, :minor1, *, *)');
     $query_response1_input->bindValue(':patient_id', $patient_id, PDO::PARAM_INT);
     $query_response1_input->bindValue(':day', $day, PDO::PARAM_INT);
     $query_response1_input->bindValue(':ampm', $ampm, PDO::PARAM_STR);
@@ -79,6 +95,36 @@ if (isset($_POST['patient_id']) && isset($_POST['day']) && isset($_POST['ampm'])
 
     if (isset($_POST['response1_input'])) {
       process_response1_input($db_connection, $patient_id, $day, $ampm);
+      if (isset($_POST['response2'])) {
+        // TODO: IMPLEMENT THIS
+        if (isset($_POST['response2_words'])) {
+          // TODO: IMPLEMENT THIS
+          if (isset($_POST['response2_input'])) {
+            // TODO: IMPLEMENT THIS
+            if (isset($_POST['response3_medications'])) {
+              // TODO: IMPLEMENT THIS
+              if (isset($_POST['response3_activity'])) {
+                // TODO: IMPLEMENT THIS
+                if (isset($_POST['response3_input'])) {
+                  // TODO: IMPLEMENT THIS
+                } else {
+                  echo "Error: response3_input not set";
+                }
+              } else {
+                echo "Error: response3_activity not set";
+              }
+            } else {
+              echo "Error: response3_medications not set";
+            }
+          } else {
+            echo "Error: response2_input not set";
+          }
+        } else {
+          echo "Error: response2_words not set";
+        }
+      } else {
+        echo "Error: response2 not set";
+      }
     } else {
       echo "Error: response1_input not set";
     }
