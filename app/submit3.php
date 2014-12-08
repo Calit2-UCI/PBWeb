10ea06c99257b708c9cab9045f94aea3f50f5efd
Tianrui Guo <tianruig@gmail.com>
2014-12-07 19:52:48 -0800

    in progress

--- app/submit3.php
+++ app/submit3.php
@@ -80,7 +80,7 @@ $query1->execute();
 function process_response3($db_connection, $patient_id, $day, $ampm)
 {
   // setup an array to write to db later
-  $query3_array = array();
+  $response3_array = array();
 
   // Deal with the first 3 medication questions
   // response3_medications: The responses to the 3 “Have you taken any pain medications since the last diary entry?” questions.
@@ -88,101 +88,258 @@ function process_response3($db_connection, $patient_id, $day, $ampm)
   // ex: 1,some medication,1,3,1,other medication,3,1,0,*,*,*
   $response3_medications = explode(",", $_POST['response3_medications']);
   for ($i = 0; $i < 3; ++$i) {
-    $query3_array[$i] = array();
+    $response3_array[$i] = array();
 
     // 1. Have you taken any pain medications since last entry?
     if (isset($response3_medications[$i * 4])) {
       if ($response3_medications[($i * 4)] == "*") {
-        $query3_array[$i][0] = 1;
+        $response3_array[$i][0] = 1;
       } elseif ($response3_medications[($i * 4)] == 0) {
-        $query3_array[$i][0] = 2;
+        $response3_array[$i][0] = 2;
       } elseif ($response3_medications[($i * 4)] == 1) {
-        $query3_array[$i][0] = 3;
+        $response3_array[$i][0] = 3;
       } else {
-        $query3_array[$i][0] = 0;
+        $response3_array[$i][0] = 0;
       }
     } else {
-      $query3_array[$i][0] = 0;
+      $response3_array[$i][0] = 0;
     }
 
     // 2. What was the name of this medication?
     if (isset($response3_medications[($i * 4 + 1)])) {
       if ($response3_medications[($i * 4 + 1)] == "*") {
-        $query3_array[$i][1] = 1;
+        $response3_array[$i][1] = 1;
       } else {
-        $query3_array[$i][1] = $response3_medications[($i * 4 + 1)];
+        $response3_array[$i][1] = $response3_medications[($i * 4 + 1)];
       }
     } else {
-      $query3_array[$i][1] = 0;
+      $response3_array[$i][1] = -2;
     }
 
-    // 3. How many times was this activity done since the last entry?
-    // 4. How much did the activity help?
+    // 3. How many times was this medication since the last entry?
+    // 4. How much did the medication help?
     for ($j = 2; $j < 4; ++$j) {
       if (isset($response3_medications[($i * 4 + $j)])) {
         if ($response3_medications[($i * 4 + $j)] == "*") {
-          $query3_array[$i][$j] = 1;
+          $response3_array[$i][$j] = 1;
         } elseif (is_numeric($response3_medications[($i * 4 + $j)])) {
-          $query3_array[$i][$j] = $response3_medications[($i * 4 + $j)] + 2;
+          $response3_array[$i][$j] = $response3_medications[($i * 4 + $j)] + 2;
         } else {
-          $query3_array[$i][$j] = 0;
+          $response3_array[$i][$j] = 0;
         }
       } else {
-        $query3_array[$i][$j] = 0;
+        $response3_array[$i][$j] = 0;
       }
     }
   }
 
 
   $response3_activity = explode(",", $_POST['response3_activity']);
-  for ($i = 0; $i < 7; ++$i) {
+  for ($i = 0; $i < 8; ++$i) {
     // 1. Have you done _____ (i.e. Deep Breathing)
     if (isset($response3_activity[$i * 4])) {
       if ($response3_activity[($i * 4)] == "*") {
-        $query3_array[($i + 3)][0] = 1;
+        $response3_array[($i + 3)][0] = 1;
       } elseif ($response3_activity[($i * 4)] == 0) {
-        $query3_array[($i + 3)][0] = 2;
+        $response3_array[($i + 3)][0] = 2;
       } elseif ($response3_activity[($i * 4)] == 1) {
-        $query3_array[($i + 3)][0] = 3;
+        $response3_array[($i + 3)][0] = 3;
       } else {
-        $query3_array[($i + 3)][0] = 0;
+        $response3_array[($i + 3)][0] = 0;
       }
     } else {
-      $query3_array[($i + 3)][0] = 0;
+      $response3_array[($i + 3)][0] = 0;
     }
-
-
-
+    
     // 3. What time was this last done?
     if (isset($response3_activity[($i * 4 + 2)])) {
       if ($response3_activity[($i * 4 + 2)] == "*") {
-        $query3_array[($i + 3)][2] = 1;
+        $response3_array[($i + 3)][2] = 1;
       } else {
-        $query3_array[($i + 3)][2] = $response3_activity[($i * 4 + 2)];
+        $response3_array[($i + 3)][2] = $response3_activity[($i * 4 + 2)];
       }
     } else {
-      $query3_array[($i + 3)][2] = 0;
+      $response3_array[($i + 3)][2] = -2;
     }
 
-    // 2. How many times was this medication taken since last entry?
-    // 4. How much did the medication help?
+    // 2. How many times was this activity done since last entry?
+    // 4. How much did the activity help?
     for ($j = 1; $j < 4; $j += 2) {
       if (isset($response3_activity[($i * 4 + $j)])) {
         if ($response3_activity[($i * 4 + $j)] == "*") {
-          $query3_array[($i + 3)][$j] = 1;
+          $response3_array[($i + 3)][$j] = 1;
         } elseif (is_numeric($response3_activity[($i * 4 + $j)])) {
-          $query3_array[($i + 3)][$j] = $response3_activity[($i * 4 + $j)] + 2;
+          $response3_array[($i + 3)][$j] = $response3_activity[($i * 4 + $j)] + 2;
         } else {
-          $query3_array[($i + 3)][$j] = 0;
+          $response3_array[($i + 3)][$j] = 0;
         }
       } else {
-        $query3_array[($i + 3)][$j] = 0;
+        $response3_array[($i + 3)][$j] = 0;
       }
     }
   }
-  
-  print_r($query3_array);
-}
+
+  $response3_input = explode(",", $_POST['response3_input']);
+  for ($i = 0; $i < 1; ++$i) {
+    // 1. Have you done any other activities
+    if (isset($response3_input[$i * 4])) {
+      if ($response3_input[($i * 4)] == "*") {
+        $response3_array[($i + 3 + 8)][0] = 1;
+      } elseif ($response3_input[($i * 4)] == 0) {
+        $response3_array[($i + 3 + 8)][0] = 2;
+      } elseif ($response3_input[($i * 4)] == 1) {
+        $response3_array[($i + 3 + 8)][0] = 3;
+      } else {
+        $response3_array[($i + 3 + 8)][0] = 0;
+      }
+    } else {
+      $response3_array[($i + 3 + 8)][0] = 0;
+    }
+
+
+    // 2. What is name of activity
+    if (isset($response3_input[($i * 4 + 1)])) {
+      if ($response3_input[($i * 4 + 1)] == "*") {
+        $response3_array[($i + 3 + 8)][1] = 1;
+      } else {
+        $response3_array[($i + 3 + 8)][1] = $response3_input[($i * 4 + 1)];
+      }
+    } else {
+      $response3_array[($i + 3 + 8)][1] = -2;
+    }
+
+    // 4. What time was this last done?
+    if (isset($response3_input[($i * 4 + 3)])) {
+      if ($response3_input[($i * 4 + 3)] == "*") {
+        $response3_array[($i + 3 + 8)][3] = 1;
+      } else {
+        $response3_array[($i + 3 + 8)][3] = $response3_input[($i * 4 + 3)];
+      }
+    } else {
+      $response3_array[($i + 3 + 8)][3] = -2;
+    }
+
+
+    // 2. How many times was this activity taken since last entry?
+    // 4. How much did the activity help?
+    for ($j = 2; $j < 5; $j += 2) {
+      if (isset($response3_input[($i * 4 + $j)])) {
+        if ($response3_input[($i * 4 + $j)] == "*") {
+          $response3_array[($i + 3 + 8)][$j] = 1;
+        } elseif (is_numeric($response3_input[($i * 4 + $j)])) {
+          $response3_array[($i + 3 + 8)][$j] = $response3_input[($i * 4 + $j)] + 2;
+        } else {
+          $response3_array[($i + 3 + 8)][$j] = 0;
+        }
+      } else {
+        $response3_array[($i + 3 + 8)][$j] = 0;
+      }
+    }
+  }
+
+  print_r($response3_array);
+
+  $response3_query = "INSERT INTO section3_intervention (patient_id, Datecomp, DayNum, ampm,
+    med1, med1name, med1num, med1help,
+    med2, med2name, med2num, med2help,
+    med3, med3name, med3num, med3help,
+    breathe, breathen, breathet, breatheh,
+    relax, relaxn, relaxt, relaxh,
+    postalk, postalkn, postalkt, postalkh,
+    heat, heatn, heatt, heath,
+    massage, massagen, massaget, massageh,
+    imagery, imageryn, imageryt, imageryh,
+    dstract, dstractn, dstractt, dstracth,
+    socsup, socsupn, socsupt, socsuph,
+    intoth, intothnm, intothn, intotht, intothh) VALUES
+    (:patient_id, :Datecomp, :DayNum, :ampm,
+    :med1, :med1name, :med1num, :med1help,
+    :med2, :med2name, :med2num, :med2help,
+    :med3, :med3name, :med3num, :med3help,
+    :breathe, :breathen, :breathet, :breatheh,
+    :relax, :relaxn, :relaxt, :relaxh,
+    :postalk, :postalkn, :postalkt, :postalkh,
+    :heat, :heatn, :heatt, :heath,
+    :massage, :massagen, :massaget, :massageh,
+    :imagery, :imageryn, :imageryt, :imageryh,
+    :dstract, :dstractn, :dstractt, :dstracth,
+    :socsup, :socsupn, :socsupt, :socsuph,
+    :intoth, :intothnm, :intothn, :intotht, :intothh)";
+
+  try {
+    $query_response3 = $db_connection->prepare($response3_query);
+    $query_response3->bindValue(':patient_id', $patient_id, PDO::PARAM_INT);
+    // TODO: insert actual date
+    $query_response3->bindValue(':Datecomp', "11112011", PDO::PARAM_STR);
+    $query_response3->bindValue(':DayNum', $day, PDO::PARAM_INT);
+    $query_response3->bindValue(':ampm', ($ampm == "am" ? 1 : 2), PDO::PARAM_STR);
+
+    $query_response3->bindValue(':med1', ($response3_array[0][0] - 2), PDO::PARAM_INT);
+    $query_response3->bindValue(':med1name', ($response3_array[0][1] ), PDO::PARAM_STR);
+    $query_response3->bindValue(':med1num', ($response3_array[0][2] - 2), PDO::PARAM_INT);
+    $query_response3->bindValue(':med1help', ($response3_array[0][3] - 2), PDO::PARAM_INT);
+
+    $query_response3->bindValue(':med2', ($response3_array[1][0] - 2), PDO::PARAM_INT);
+    $query_response3->bindValue(':med2name', ($response3_array[1][1] ), PDO::PARAM_STR);
+    $query_response3->bindValue(':med2num', ($response3_array[1][2] - 2), PDO::PARAM_INT);
+    $query_response3->bindValue(':med2help', ($response3_array[1][3] - 2), PDO::PARAM_INT);
+
+    $query_response3->bindValue(':med3', ($response3_array[2][0] - 2), PDO::PARAM_INT);
+    $query_response3->bindValue(':med3name', ($response3_array[2][1] ), PDO::PARAM_STR);
+    $query_response3->bindValue(':med3num', ($response3_array[2][2] - 2), PDO::PARAM_INT);
+    $query_response3->bindValue(':med3help', ($response3_array[2][3] - 2), PDO::PARAM_INT);
+
+    $query_response3->bindValue(':breathe', ($response3_array[3][0] - 2), PDO::PARAM_INT);
+    $query_response3->bindValue(':breathen', ($response3_array[3][1] - 2), PDO::PARAM_INT);
+    $query_response3->bindValue(':breathet', ($response3_array[3][2] ), PDO::PARAM_STR);
+    $query_response3->bindValue(':breatheh', ($response3_array[3][3] - 2), PDO::PARAM_INT);
+
+    $query_response3->bindValue(':relax', ($response3_array[4][0] - 2), PDO::PARAM_INT);
+    $query_response3->bindValue(':relaxn', ($response3_array[4][1] - 2), PDO::PARAM_INT);
+    $query_response3->bindValue(':relaxt', ($response3_array[4][2]), PDO::PARAM_STR);
+    $query_response3->bindValue(':relaxh', ($response3_array[4][3] - 2), PDO::PARAM_INT);
+
+    $query_response3->bindValue(':postalk', ($response3_array[5][0] - 2), PDO::PARAM_INT);
+    $query_response3->bindValue(':postalkn', ($response3_array[5][1] - 2), PDO::PARAM_INT);
+    $query_response3->bindValue(':postalkt', ($response3_array[5][2]), PDO::PARAM_STR);
+    $query_response3->bindValue(':postalkh', ($response3_array[5][3] - 2), PDO::PARAM_INT);
+
+    $query_response3->bindValue(':heat', ($response3_array[6][0] - 2), PDO::PARAM_INT);
+    $query_response3->bindValue(':heatn', ($response3_array[6][1] - 2), PDO::PARAM_INT);
+    $query_response3->bindValue(':heatt', ($response3_array[6][2] ), PDO::PARAM_STR);
+    $query_response3->bindValue(':heath', ($response3_array[6][3] - 2), PDO::PARAM_INT);
+
+    $query_response3->bindValue(':massage', ($response3_array[7][0] - 2), PDO::PARAM_INT);
+    $query_response3->bindValue(':massagen', ($response3_array[7][1] - 2), PDO::PARAM_INT);
+    $query_response3->bindValue(':massaget', ($response3_array[7][2] ), PDO::PARAM_STR);
+    $query_response3->bindValue(':massageh', ($response3_array[7][3] - 2), PDO::PARAM_INT);
+
+    $query_response3->bindValue(':imagery', ($response3_array[8][0] - 2), PDO::PARAM_INT);
+    $query_response3->bindValue(':imageryn', ($response3_array[8][1] - 2), PDO::PARAM_INT);
+    $query_response3->bindValue(':imageryt', ($response3_array[8][2] ), PDO::PARAM_STR);
+    $query_response3->bindValue(':imageryh', ($response3_array[8][3] - 2), PDO::PARAM_INT);
+
+    $query_response3->bindValue(':dstract', ($response3_array[9][0] - 2), PDO::PARAM_INT);
+    $query_response3->bindValue(':dstractn', ($response3_array[9][1] - 2), PDO::PARAM_INT);
+    $query_response3->bindValue(':dstractt', ($response3_array[9][2] ), PDO::PARAM_STR);
+    $query_response3->bindValue(':dstracth', ($response3_array[9][3] - 2), PDO::PARAM_INT);
+
+    $query_response3->bindValue(':socsup', ($response3_array[10][0] - 2), PDO::PARAM_INT);
+    $query_response3->bindValue(':socsupn', ($response3_array[10][1] - 2), PDO::PARAM_INT);
+    $query_response3->bindValue(':socsupt', ($response3_array[10][2]), PDO::PARAM_STR);
+    $query_response3->bindValue(':socsuph', ($response3_array[10][3] - 2), PDO::PARAM_INT);
+
+    $query_response3->bindValue(':intoth', ($response3_array[11][0] - 2), PDO::PARAM_INT);
+    $query_response3->bindValue(':intothnm', ($response3_array[11][1]), PDO::PARAM_STR);
+    $query_response3->bindValue(':intothn', ($response3_array[11][2] - 2), PDO::PARAM_INT);
+    $query_response3->bindValue(':intotht', ($response3_array[11][3]), PDO::PARAM_STR);
+    $query_response3->bindValue(':intothh', ($response3_array[11][4] - 2), PDO::PARAM_INT);
+
+    $query_response3->execute();
+  } catch (Exception $e) {
+    echo($e->getMessage());
+  }
+  }
 
 if (isset($_POST['patient_id']) && isset($_POST['day']) && isset($_POST['ampm'])) {
   $patient_id = $_POST['patient_id'];
