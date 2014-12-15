4da60051e26a4ed11ff3f3a58b6cd0e35dcf76cf
Tianrui Guo <tianruig@gmail.com>
2014-12-14 16:20:22 -0800

    WIP: Emailing

--- app/alert_handling.php
+++ app/alert_handling.php
@@ -1,6 +1,9 @@
 <?php
 
 require_once('../config/config.php');
+require_once('../libraries/PHPMailer.php');
+
+$db_connection = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8', DB_USER, DB_PASS);
 
 function sendEmail($user_email, $subject, $message)
 {
@@ -42,5 +45,26 @@ function sendEmail($user_email, $subject, $message)
     return false;
   } else {
     return true;
+  }
+}
+
+if (isset($_POST['patient_id']) && isset($_POST['alerts'])) {
+  try {
+    $query_email = $db_connection->prepare("SELECT user_email FROM users WHERE user_id=(SELECT doctor_id FROM patients WHERE patient_id=:patient_id)");
+    $query_email->bindValue(':patient_id', $_POST['patient_id'], PDO::PARAM_INT);
+    $query_email->execute();
+    $email = $query_email->fetch()[0];
+  } catch (Exception $e) {
+    echo($e->getMessage());
+  }
+
+
+  $message = "Alert: Patient " . $_POST['patient_id'] . " who is enrolled in the Pain Buddy study has had
+      _________________. Please contact patient as soon
+      as possible to follow up. You can follow this link to review the patient’s symptoms and information:
+      insertlinkhere.com";
+
+  if (isset($email)) {
+    sendEmail($email, "Painbuddy Alert", $message);
   }
 }
