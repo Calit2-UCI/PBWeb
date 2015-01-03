d01a3944e5f63b9d93159e9c0043bb2b61bff72f
Tianrui Guo <tianruig@gmail.com>
2015-01-03 13:36:06 -0800

    Email Alerts
    App sends message to app/submit_alerts.php with the error string and
    other information. The alerts are written into a database.
    app/send_emails.php queries this database and sends out emails to alert
    the HCP

---
+++ app/send_emails.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * This script checks the HCP alerts table and sends out alert emails
+ */
+
+require_once('../config/config.php');
+require_once('../libraries/PHPMailer.php');
+
+$db_connection = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8', DB_USER, DB_PASS);
+
+function sendEmail($user_email, $subject, $message)
+{
+    $mail = new PHPMailer;
+
+    // please look into the config/config.php for much more info on how to use this!
+    // use SMTP or use mail()
+    if (EMAIL_USE_SMTP) {
+        // Set mailer to use SMTP
+        $mail->IsSMTP();
+        //useful for debugging, shows full SMTP errors
+        //$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+        // Enable SMTP authentication
+        $mail->SMTPAuth = EMAIL_SMTP_AUTH;
+        // Enable encryption, usually SSL/TLS
+        if (defined(EMAIL_SMTP_ENCRYPTION)) {
+            $mail->SMTPSecure = EMAIL_SMTP_ENCRYPTION;
+        }
+        // Specify host server
+        $mail->Host = EMAIL_SMTP_HOST;
+        $mail->Username = EMAIL_SMTP_USERNAME;
+        $mail->Password = EMAIL_SMTP_PASSWORD;
+        $mail->Port = EMAIL_SMTP_PORT;
+    } else {
+        $mail->IsMail();
+    }
+
+    $mail->From = EMAIL_VERIFICATION_FROM;
+    $mail->FromName = EMAIL_VERIFICATION_FROM_NAME;
+    $mail->AddAddress($user_email);
+    $mail->Subject = $subject;
+
+    // the link to your register.php, please set this value in config/email_verification.php
+    $mail->Body = $message;
+
+    if (!$mail->Send()) {
+        // TODO: log this to a file somewhere
+        echo "Email error: " . $mail->ErrorInfo;
+        return false;
+    } else {
+        return true;
+    }
+}
+
+try {
+    $query_alerts = $db_connection->prepare("SELECT a.id, a.patient_id, d.first_name, d.last_name, b.message, c.user_email from HCP_alerts a
+              INNER JOIN alert_codes b ON a.age_group=b.age_group AND a.code=b.alert_code
+              INNER JOIN patients d ON a.patient_id=d.patient_id
+              INNER JOIN users c ON d.doctor_id=c.user_id
+              WHERE a.email_sent=0");
+    $query_alerts->execute();
+    $alerts = $query_alerts->fetchAll();
+
+    foreach ($alerts as $alert) {
+        $message = "Alert: Patient " . $alert['first_name'] . " " . $alert['last_name'] . " (ID: " . $alert['patient_id'] . ") who is enrolled in the Pain Buddy study has had " . $alert['message'] . ". Please contact patient as soon as possible to follow up. You can follow this link to review the patient’s symptoms and information: http://buddy.calplug.uci.edu/patient_details.php?patient_id=" . $alert['patient_id'];
+
+        echo $alert['user_email'];
+        echo $message;
+        echo "\n";
+
+        if (sendEmail($alert['user_email'], "Painbuddy Alert", $message)){
+            $query = $db_connection->prepare("UPDATE HCP_alerts SET email_sent=1 WHERE id=:id");
+            $query->bindParam(':id', $alert['id'], PDO::PARAM_INT);
+            $query->execute();
+        }
+    }
+
+} catch (Exception $e) {
+    echo($e->getMessage());
+}
---
+++ app/submit_alerts.php
@@ -0,0 +1,101 @@
+<?php
+
+/**
+ * App sends all alert messages to this script, which writes then into the database for the HCP to view
+ * Emails are then sent to the HCP
+ *
+ * POST Parameters:
+ * patient_id: id of the patient
+ * patient_age: age of the patient
+ * alerts: string of alert codes, separated by commas.
+ * day: day number of survey which triggered the alert
+ * ampm: whether the survey was in the am or pm
+ *
+ * CREATE TABLE IF NOT EXISTS `painbuddy`.`HCP_alerts` (
+ * `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT 'unique id for each alert',
+ * `patient_id` INT(11) NOT NULL COMMENT 'ID of the patient',
+ * `DayNum` INT(11) NOT NULL COMMENT 'day number',
+ * `ampm` TINYINT NOT NULL COMMENT 'am or pm survey (1=AM, 2=PM)',
+ * `age_group` VARCHAR(1) NOT NULL COMMENT 'a=10 to 18, b=8 to 9',
+ * `code` VARCHAR(10) NOT NULL COMMENT 'alert code (see alert_codes table)',
+ * `email_sent` BOOLEAN DEFAULT '0' COMMENT 'did we send an email about this alert to hcp?',
+ * `hcp_acknowledged` BOOLEAN DEFAULT '0' COMMENT 'has the hcp acknowledged the alert?',
+ * PRIMARY KEY (`id`)
+ * ) AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='patient alerts'
+ *
+ * CREATE TABLE alert_codes (id INT(11) AUTO_INCREMENT PRIMARY KEY, age_group VARCHAR(1) CHARACTER SET utf8,alert_code VARCHAR(10) CHARACTER SET utf8,message VARCHAR(255) CHARACTER SET utf8);
+ * INSERT INTO alert_codes (age_group, alert_code, message) VALUES ('a','p/1','Pain');
+ * INSERT INTO alert_codes (age_group, alert_code, message) VALUES ('a','d/1','Drowsiness');
+ * INSERT INTO alert_codes (age_group, alert_code, message) VALUES ('a','pu/1','Problems with urination');
+ * INSERT INTO alert_codes (age_group, alert_code, message) VALUES ('a','s/1','Shortness of breath');
+ * INSERT INTO alert_codes (age_group, alert_code, message) VALUES ('a','ds/1','Difficulty Swallowing');
+ * INSERT INTO alert_codes (age_group, alert_code, message) VALUES ('a','c/3','3 consecutive entries of Cough');
+ * INSERT INTO alert_codes (age_group, alert_code, message) VALUES ('a','n/3','3 consecutive entries of Numbness/Tingling');
+ * INSERT INTO alert_codes (age_group, alert_code, message) VALUES ('a','v/3','3 consecutive entries of Vomiting');
+ * INSERT INTO alert_codes (age_group, alert_code, message) VALUES ('a','na/3','3 consecutive entries of Nausea');
+ * INSERT INTO alert_codes (age_group, alert_code, message) VALUES ('a','d/3','3 consecutive entries of Diarrhea');
+ * INSERT INTO alert_codes (age_group, alert_code, message) VALUES ('a','dz/3','3 consecutive entries of Dizziness');
+ * INSERT INTO alert_codes (age_group, alert_code, message) VALUES ('a','i/3','3 consecutive entries of Itching ');
+ * INSERT INTO alert_codes (age_group, alert_code, message) VALUES ('a','cs/3','3 consecutive entries of Constipation');
+ * INSERT INTO alert_codes (age_group, alert_code, message) VALUES ('a','h/3','3 consecutive entries of Headache');
+ * INSERT INTO alert_codes (age_group, alert_code, message) VALUES ('a','m/3','3 consecutive entries of Mouth sores');
+ * INSERT INTO alert_codes (age_group, alert_code, message) VALUES ('a','l/5','5 consecutive entries of Lack of Energy');
+ * INSERT INTO alert_codes (age_group, alert_code, message) VALUES ('a','n/5','5 consecutive entries of Nervousness');
+ * INSERT INTO alert_codes (age_group, alert_code, message) VALUES ('a','f/5','5 consecutive entries of Feeling of sadness');
+ * INSERT INTO alert_codes (age_group, alert_code, message) VALUES ('a','s/5','5 consecutive entries of Sweats');
+ * INSERT INTO alert_codes (age_group, alert_code, message) VALUES ('a','sal/5','5 consecutive entries of Swelling in arms or legs');
+ * INSERT INTO alert_codes (age_group, alert_code, message) VALUES ('a','c/5','5 consecutive entries of Changes in skin');
+ * INSERT INTO alert_codes (age_group, alert_code, message) VALUES ('b','p/1','Pain');
+ * INSERT INTO alert_codes (age_group, alert_code, message) VALUES ('b','t/3','3 consecutive entries of Tired');
+ * INSERT INTO alert_codes (age_group, alert_code, message) VALUES ('b','i/3','3 consecutive entries of Itching ');
+ * INSERT INTO alert_codes (age_group, alert_code, message) VALUES ('b','v/3','3 consecutive entries of Vomiting');
+ * INSERT INTO alert_codes (age_group, alert_code, message) VALUES ('b','s/5','5 consecutive entries of Sleep');
+ * INSERT INTO alert_codes (age_group, alert_code, message) VALUES ('b','f/5','5 consecutive entries of Feeling of sad');
+ * INSERT INTO alert_codes (age_group, alert_code, message) VALUES ('b','w/5','5 consecutive entries of Worried');
+ */
+
+
+require_once(dirname(__FILE__) . '/../config/config.php');
+require_once(dirname(__FILE__) . '/../libraries/PHPMailer.php');
+
+$db_connection = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8', DB_USER, DB_PASS);
+//TODO: remove
+$db_connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// TODO: do some checking of the POST parameters
+$patient_id = $_POST['patient_id'];
+$age_group = $_POST['patient_age'] < 10 ? "b" : "a";
+$day = $_POST['day'];
+$ampm = $_POST['ampm'];
+
+// Do some checking of the alert string
+$alert_array = explode(",", $_POST['alerts']);
+if ($age_group == "b") {
+    $codes = array('p/1', 't/3', 'i/3', 'v/3', 's/5', 'f/5', 'w/5');
+} else {
+    $codes = array('p/1', 'd/1', 'pu/1', 's/1', 'ds/1', 'c/3', 'n/3', 'v/3', 'na/3', 'd/3', 'dz/3', 'i/3', 'cs/3', 'h/3', 'm/3', 'l/5', 'n/5', 'f/5', 's/5', 'sal/5', 'c/5');
+}
+
+foreach ($alert_array as $alert_code) {
+    if (!in_array($alert_code, $codes)) {
+        die($alert_code . " not a valid code");
+    } else {
+        try {
+            $query = $db_connection->prepare('INSERT INTO HCP_alerts (`patient_id`, `dayNum`, `ampm`, `age_group`, `code`) VALUES (:patient_id, :dayNum, :ampm, :age_group, :code)');
+            $query->bindValue(':patient_id', $patient_id, PDO::PARAM_INT);
+            $query->bindValue(':dayNum', $day, PDO::PARAM_INT);
+            $query->bindValue(':ampm', ($ampm == "am" ? 1 : 2), PDO::PARAM_STR);
+            $query->bindValue(':age_group', $age_group, PDO::PARAM_INT);
+            $query->bindValue(':code', $alert_code, PDO::PARAM_STR);
+            $query->execute();
+            if ($query->rowCount() > 0) {
+                echo "Alert messages written to table. Emails will now be sent";
+            } else {
+                echo "An error occurred while writing codes to database";
+            }
+        } catch (Exception $e) {
+            echo($e->getMessage());
+        }
+    }
+}
+
